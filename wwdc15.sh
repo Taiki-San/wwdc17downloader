60041258e6e89e241f0c97b1d31e433df1ff6c66
Taiki <taiki@rakshata.com>
2015-06-14 03:05:18 +0200

    Add ability to resume an interrupted download

--- wwdc15.sh
+++ wwdc15.sh
@@ -12,13 +12,13 @@ cat index.html | grep "\t<a href=\"?id=" | sed -e 's/<a href=//' -e 's#/a>##' -e
 
 #Download dedicated webpages
 rm index.html
-cat ../download_data | awk '{ split($0, a, "\""); system( "wget \"https://developer.apple.com/videos/wwdc/2015/"a[2]"\" -O \"" a[3] "\"")}'
+cat ../download_data | awk '{ split($0, a, "\""); system( "wget -c \"https://developer.apple.com/videos/wwdc/2015/"a[2]"\" -O \"" a[3] "\"")}'
 
 #Final download
 for file in *
 do
 var=$(cat "$file" | grep "_hd_" | awk '{ split($0, a, "dl=1"); split(a[2], b, "href=\""); $1=b[2]; print substr($1, 0, length($1)-1)}')
-wget "$var" -O "../"$file".mp4"
+wget -c "$var" -O "../$file.mp4"
 done
 
 #cleanup
