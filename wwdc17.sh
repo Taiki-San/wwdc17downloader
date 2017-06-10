3b8718d45e168a880130a532c59f14243caff013
Taiki <Taiki-San@users.noreply.github.com>
2017-06-10 09:24:49 +0200

    Bring back the changes by @radianttap into the main project
    Will enable next year's script to download PDFs, and keeping the very nice `data-released` trick into my sleeve if it still works next year and if missing unreleased talks in worth it.

--- wwdc17.sh
+++ wwdc17.sh
@@ -11,6 +11,11 @@ echo "Downloading the index"
 wget -q https://developer.apple.com/videos/wwdc2017/ -O index.html
 cat index.html | grep videos/play/wwdc2017 | sed -e 's/[[:blank:]]*//' | sort | uniq | sed -e 's/.*wwdc2017\///' -e 's/\/\"\>//' > ../downloadData
 
+#cleaner way to get only released talk numbers.
+#	find parts of the document where data-released=true, all the way to the first H4 header where title of that talk is 
+#	then find lines containing "videos/play/wwdc2017", then remove all chars except session number, then clean duplicated lines
+#cat index.html | sed -n '/data-released=\"true\"/,/\<h4/p' | grep videos/play/wwdc2017 | sed -e 's/.*wwdc2017\///' -e 's/\/\"\>//' | sed '$!N; /^\(.*\)\n\1$/!P; D' > ../downloadData
+
 rm index.html
 
 #Iterate through the talk IDs
@@ -25,17 +30,25 @@ do
 	#We grep "_hd_" which bring up the download URL, then some cleanup
 	#If we were to want SD video, all we would have to do is replace _hd_ by _sd_
 	dlURL=$(cat webpage | grep _hd_ | sed -e "s/.*href\=//" -e "s/\>.*//" -e "s/\"//g")
+	pdfURL=$(cat webpage | grep .pdf | grep devstreaming | sed -e "s/.*href\=//" -e "s/\>.*//" -e "s/\"//g"  -e "s/ .*$//g")
 
 	rm webpage
 
 	#Is there a video URL?
 	if [ -z "$dlURL" ] ; then
 		echo "Video $line ($talkName) doesn't appear to be available for now"
-		continue
+	else
+		#Great, we download the file
+		wget -c "$dlURL" -O "../$line - $talkName.mp4"
 	fi
 
-	#Great, we download the file
-	wget -c "$dlURL" -O "../$line - $talkName.mp4"
+	#Is there a PDF URL?
+	if [ -z "$pdfURL" ]; then
+		echo "Slides for $line ($talkName) don't appear to be available for now"
+	else
+		#Great, we download the file
+		wget -c "$pdfURL" -O "../$line - $talkName.pdf"
+	fi
 
 done < "../downloadData"
 
